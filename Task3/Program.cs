d1a26e7c112d4273d21f866c7db6516abd5de657
SvZor <zorinasp@yandex.ru>
2024-03-10 02:41:20 +0900

    задача2

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// задача 3. считать строку с консоли, состоящую из
+// цифр и латинских букв.
+// Сформировать новую строку, состоящую из букв исходной строки.
+// Этапы решения:
+// 1 ввод строки 123pasaw0rd
+// 2 проход по всем элементам строки
+// 3 проверка элемента: является ли он буквой?
+// используем метод is letter?
+// 4 дописать подходящий элемент к новой строке
+
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
